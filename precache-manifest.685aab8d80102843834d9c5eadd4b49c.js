6a0b2a143b8ae57105f8df3f837ce3e2030fd01c
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-12-13 19:10:19 +0300

    Updates

---
+++ precache-manifest.685aab8d80102843834d9c5eadd4b49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd970c384b46669de4fa6a75f9ae7545",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "3a4dd2cfaa2e938cd4f4",
+    "url": "/todolist-ts/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "6bd48036cf14418cb9f9",
+    "url": "/todolist-ts/static/js/2.b3152d09.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts/static/js/2.b3152d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4dd2cfaa2e938cd4f4",
+    "url": "/todolist-ts/static/js/main.4288245a.chunk.js"
+  },
+  {
+    "revision": "8bdf15c7027486d2fcd1",
+    "url": "/todolist-ts/static/js/runtime-main.ca93a274.js"
+  }
+]);
